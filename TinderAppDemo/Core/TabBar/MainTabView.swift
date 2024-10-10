19da262405b25f1abb4e21827bf7704731c27f6a
antoineB <bernardh2o.antoine@gmail.com>
2024-10-10 10:09:23 +0200

    Create TabBar modul

---
+++ TinderAppDemo/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  TinderAppDemo
+//
+//  Created by Antoine on 10/10/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
