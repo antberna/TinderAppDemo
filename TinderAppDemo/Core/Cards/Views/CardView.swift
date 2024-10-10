489ea9d8fb19e645c61bb593bcbfaa07101c36b2
antoineB <bernardh2o.antoine@gmail.com>
2024-10-10 10:53:09 +0200

    Add the first image profile

---
+++ TinderAppDemo/Core/Cards/Views/CardView.swift
@@ -0,0 +1,35 @@
+//
+//  CardView.swift
+//  TinderAppDemo
+//
+//  Created by Antoine on 10/10/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+
+    var body: some View {
+        ZStack {
+            Image(.jane1)
+                .resizable()
+                .scaledToFill()
+                .frame(width: cardWidth, height: cardHeight)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+    }
+}
+
+private extension CardView {
+    var cardWidth: CGFloat {
+        UIScreen.main.bounds.width - 20
+    }
+    
+    var cardHeight: CGFloat {
+        UIScreen.main.bounds.height / 1.45
+    }
+}
+
+#Preview {
+    CardView()
+}
