dc156802477df56fcfdd1574bf3b27f122a2efdf
Antoine Bernardeau <bernardh2o.antoine@gmail.com>
2024-10-10 09:32:45 +0200

    Initial Commit

---
+++ TinderAppDemo/TinderAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderAppDemoApp.swift
+//  TinderAppDemo
+//
+//  Created by Antoine on 10/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TinderAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TinderAppDemoTests/TinderAppDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TinderAppDemoTests.swift
+//  TinderAppDemoTests
+//
+//  Created by Antoine on 10/10/2024.
+//
+
+import Testing
+@testable import TinderAppDemo
+
+struct TinderAppDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
