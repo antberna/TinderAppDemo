2255135f24f8b1d8f71c62af513619ba30b190e8
antoineB <bernardh2o.antoine@gmail.com>
2024-10-10 11:10:45 +0200

    Create the botom card ( user information)

---
+++ TinderAppDemo/Core/Cards/Views/UserInfoViews.swift
@@ -0,0 +1,18 @@
+//
+//  UserInfoViews.swift
+//  TinderAppDemo
+//
+//  Created by Antoine on 10/10/2024.
+//
+
+import SwiftUI
+
+struct UserInfoViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserInfoViews()
+}
